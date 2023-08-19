fef2c27fb0a5982c979eb7cb3cb9870c6410fd1d
Saurabh Sharma <jztsaurabh@gmail.com>
2023-08-19 15:56:47 +0530

    chapter complete

--- compiler.c
+++ compiler.c
@@ -4,29 +4,33 @@
 #include "compiler.h"
 #include "scanner.h"
 
-void compile(const char *source)
+void compile(const char *source, Chunk *chunk)
 {
   initScanner(source);
-  int line = -1;
+  advance();
+  expression();
+  // START FROM HERE https://craftinginterpreters.com/compiling-expressions.html
+  consume(TOKEN_EOF, "Expect end of expression.");
 
-  for (;;)
-  {
-    Token token = scanToken();
-    if (token.line != line)
-    {
-      printf("%4d ", token.line);
-      line = token.line;
-    }
-    else
-    {
-      printf("    | ");
-    }
+  // int line = -1;
+  // for (;;)
+  // {
+  //   Token token = scanToken();
+  //   if (token.line != line)
+  //   {
+  //     printf("%4d ", token.line);
+  //     line = token.line;
+  //   }
+  //   else
+  //   {
+  //     printf("    | ");
+  //   }
 
-    printf("%2d '%s.*s'\n", token.type, token.length, token.start);
+  //   printf("%2d '%s.*s'\n", token.type, token.length, token.start);
 
-    if (token.type == TOKEN_EOF)
-    {
-      break;
-    }
-  }
+  //   if (token.type == TOKEN_EOF)
+  //   {
+  //     break;
+  //   }
+  // }
 }
