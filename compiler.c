aaed001d91f3824fbb5d70537d545e422f019040
Saurabh Sharma <jztsaurabh@gmail.com>
2023-09-02 18:12:05 +0530

    added string support

--- compiler.c
+++ compiler.c
@@ -4,6 +4,7 @@
 #include "common.h"
 #include "compiler.h"
 #include "scanner.h"
+#include "object.h"
 
 #ifndef DEBUG_PRINT_CODE
 #include "debug.h"
@@ -263,6 +264,11 @@ static void binary()
   }
 }
 
+static void string()
+{
+  emitConstant(OBJ_VAL(copyString(parser.previous.start + 1, parser.previous.length - 2)));
+}
+
 static void parsePrecedence(Precedence precedence)
 {
   advance();
@@ -304,7 +310,7 @@ ParseRule rules[] = {
     [TOKEN_LESS] = {NULL, binary, PREC_COMPARISON},
     [TOKEN_LESS_EQUAL] = {NULL, binary, PREC_COMPARISON},
     [TOKEN_IDENTIFIER] = {NULL, NULL, PREC_NONE},
-    [TOKEN_STRING] = {NULL, NULL, PREC_NONE},
+    [TOKEN_STRING] = {string, NULL, PREC_NONE},
     [TOKEN_NUMBER] = {number, NULL, PREC_NONE},
     [TOKEN_AND] = {NULL, NULL, PREC_NONE},
     [TOKEN_CLASS] = {NULL, NULL, PREC_NONE},
