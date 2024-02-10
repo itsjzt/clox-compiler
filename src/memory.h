739aecd44bea224d6900c048976d2946fe28c933
Saurabh Sharma <jztsaurabh@gmail.com>
2024-02-10 09:45:00 +0530

    refactor folder structure

--- src/memory.h
+++ src/memory.h
@@ -4,8 +4,10 @@
 #include "common.h"
 #include "object.h"
 
+#define FREE(type, pointer) reallocate(pointer, sizeof(type), 0)
+
 #define GROW_CAPACITY(capacity) \
-  ((capacity) < 8 ? 8 : (capacity)*2)
+  ((capacity) < 8 ? 8 : (capacity) * 2)
 
 #define GROW_ARRAY(type, pointer, oldCount, newCount)    \
   (type *)reallocate(pointer, sizeof(type) * (oldCount), \
